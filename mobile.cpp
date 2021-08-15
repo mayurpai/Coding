6cf5f519ba87f321f614163b29df9f18c8a7b09a
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-15 20:10:46 +0530

    Coding

--- mobile.cpp
+++ mobile.cpp
@@ -7,21 +7,23 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    int valid = 0, invalid = 0, i = 0;
+    int valid = 0;
+    int invalid = 0;
+    int i = 0;
+    int count = 0;
     string mobileNums[5];
     const regex pattern("[0-9]{10}");
-    cin >> mobileNums[0];
-    // while (strcmp(mobileNums[i].c_str(), "q") != 0 && strcmp(mobileNums[i].c_str(), "Q") != 0)
-    for (int i = 1; i < 5; i++)
+    while (strcmp(mobileNums[i].c_str(), "q") != 0)
     {
+        count++;
         cin >> mobileNums[i];
         if (regex_match(mobileNums[i], pattern))
             continue;
         else
             invalid++;
     }
-    if (i < 5)
-        cout << invalid;
+    if (--count <= 5)
+        cout << --invalid;
     else
         cout << "INPUT LIMIT IS 5";
     return 0;
