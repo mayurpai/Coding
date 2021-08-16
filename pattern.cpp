fb2c8a02cf7aefd70001c7f93dc06d79bed4ceb8
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-16 18:02:18 +0530

    Coding

--- pattern.cpp
+++ pattern.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main()
 {
-    int rows, i, j, space, temp;
-    cin >> rows;
-    for (i = rows; i >= 1; --i)
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int i, n, j;
+    cin >> n;
+    for (i = 0; i < n; i++)
     {
-        temp = i;
-        for (space = 0; space < rows - i; ++space)
+        for (j = n; j > n - i; j--)
             cout << " ";
-        for (j = i; j <= 2 * i - 1; ++j)
-        {
-            cout << temp;
-            temp--;
-        }
-        for (j = 2; j <= i; ++j)
+        for (j = n - i; j >= 1; j--)
             cout << j;
+        int k = 2;
+        for (j = 0; j < n - i - 1; j++)
+            cout << k++;
         cout << endl;
     }
-    return 0;
 }
