0d586360726fd6999027707429e023e9f1dac7c7
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-19 09:24:11 +0530

    Coding

--- numberSeries3.cpp
+++ numberSeries3.cpp
@@ -6,23 +6,11 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int n, i;
+    int n;
     cin >> n;
-    int a[n];
-    a[0] = 3;
-    a[1] = 8;
-    for (i = 2; i < n; i++)
-    {
-        if (i % 2 == 0)
-        {
-            a[i] = a[i - 2] + 3;
-        }
-        else
-            a[i] = a[i - 2] + 3;
-    }
     if (n % 2 == 0)
-        cout << a[n - 1] << "";
+        cout << 8 + (3 * (n - 2) / 2);
     else
-        cout << a[n - 1] << "";
+        cout << 3 * (n / 2 + 1);
     return 0;
 }
