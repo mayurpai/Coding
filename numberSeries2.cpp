d5f7824bf88c454fe8a53f000ac8b1bb5cc6e6af
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-20 11:26:25 +0530

    Coding

--- numberSeries2.cpp
+++ numberSeries2.cpp
@@ -6,23 +6,11 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int n, i;
+    int n;
     cin >> n;
-    if (n == 1 || n == 2)
-    {
-        cout << 0;
-        exit(0);
-    }
-    int a[n];
-    a[0] = 0;
-    a[1] = 0;
-    for (i = 2; i < n; i++)
-    {
-        if (i % 2 == 0)
-            a[i] = i / 2 + 7;
-        else
-            a[i] = a[i - 2] + 6;
-    }
-    cout << a[n - 1];
+    if (n % 2 == 0)
+        cout << 6 * (n / 2 - 1);
+    else
+        cout << 7 * (n / 2);
     return 0;
 }
