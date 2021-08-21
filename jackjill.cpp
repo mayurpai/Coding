6702aefe043478a87116c02aa4c07816cb93944d
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-21 23:48:49 +0530

    Coding

--- jackjill.cpp
+++ jackjill.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int equal(int temp[], int arr[], int n)
-// {
-//     int flag = 0;
-//     for (int i = 0; i < n; i++)
-//         temp[i] == arr[i] ? flag = 1 : flag = 0;
-//     return flag;
-// }
+int equal(int temp[], int arr[], int n)
+{
+    int flag = 0;
+    for (int i = 0; i < n; i++)
+        temp[i] == arr[i] ? flag = 1 : flag = 0;
+    return flag;
+}
 int main()
 {
     int count = 0;
     int arr[] = {2, 3};
-    vector<int> v;
     int n = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < n; i++)
-    // int temp[sizeof(arr) / sizeof(arr[0])];
-    // memset(temp, 0, sizeof(arr));
-    // int n = sizeof(temp) / sizeof(temp[0]);
-    // while (equal(temp, arr, n))
+    int temp[n];
+    memset(temp, 1 /, sizeof(arr));
+    // while (!equal(temp, arr, n))
     // {
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         temp[] temp[i++]
-    //     }
+    for (int i = 0; i < n; i++)
+    {
+        // temp[i] = temp[i] + 1;
+        // count++;
+        cout << temp[i] << endl;
+    }
     // }
+    cout << count - 1;
     return 0;
 }
+
+/*INCOMPLETE CODE*/
