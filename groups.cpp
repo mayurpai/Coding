1c30f11833ebac89e4a3ba10213a2a5f6a221cfc
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-08-21 23:06:32 +0530

    Coding

--- groups.cpp
+++ groups.cpp
@@ -2,30 +2,27 @@
 using namespace std;
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    int n, temp;
+    int n, temp, flag = 1;
     cin >> n;
     if (n >= 100 && n <= 200)
     {
         if (n % 2 == 0)
         {
+            flag = 0;
             n = n / 4;
-            for (int i = 1; i <= 4; i++)
-                cout << n << endl;
         }
         else
         {
             temp = n;
             temp = temp / 4;
             n = n % 4;
-            for (int i = 1; i <= 3; i++)
-                cout << temp << endl;
-            cout << (temp + n) << endl;
         }
+        for (int i = 1; i <= 3; i++)
+            cout << temp << endl;
+        if (flag)
+            cout << temp + n;
+        else
+            cout << n;
     }
     else
         cout << "INVALID INPUT";
---
+++ jackjill.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int equal(int temp[], int arr[], int n)
+// {
+//     int flag = 0;
+//     for (int i = 0; i < n; i++)
+//         temp[i] == arr[i] ? flag = 1 : flag = 0;
+//     return flag;
+// }
+int main()
+{
+    int count = 0;
+    int arr[] = {2, 3};
+    vector<int> v;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
+    // int temp[sizeof(arr) / sizeof(arr[0])];
+    // memset(temp, 0, sizeof(arr));
+    // int n = sizeof(temp) / sizeof(temp[0]);
+    // while (equal(temp, arr, n))
+    // {
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         temp[] temp[i++]
+    //     }
+    // }
+    return 0;
+}
