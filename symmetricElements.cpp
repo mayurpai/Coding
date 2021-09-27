5fc4a3ba80e59e46f7571078c9ab4a8eebd1e902
mayurpai <mayurpai.is18@sahyadri.edu.in>
2021-09-27 16:24:04 +0530

    Coding

--- symmetricElements.cpp
+++ symmetricElements.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int n;
     cin >> n;
     int a[n][2];
@@ -12,10 +16,10 @@ int main()
             cin >> a[i][j];
     for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= 2; j++)
+        for (int j = 0; j < 2; j++)
         {
             if ((a[i][0] == a[j][1]) && (a[i][1] == a[j][0]))
-                cout << "{" << a[i][0] << "," << a[j][0] << "}" << endl;
+                cout << "{" << a[j][0] << "," << a[i][0] << "}" << endl;
         }
     }
     return 0;
